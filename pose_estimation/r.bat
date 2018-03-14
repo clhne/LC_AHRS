068783b9f9c329b258b102ef72661e9db2c991b7
lucl1 <luchenglin@leadcoretech.com>
2018-03-14 13:37:39 +0800

    [merge] n and r.bat to r.bat

--- pose_estimation/r.bat
+++ pose_estimation/r.bat
@@ -1,4 +1,7 @@
-rem @echo off
+echo off
+cls
+rem call ndk-build
+call ndk-build.cmd
 adb push libs/armeabi-v7a/pose_estimation /data/local/tmp
 adb shell chmod 777 /data/local/tmp/pose_estimation
 adb shell LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/system/lib/egl:/data/local/tmp /data/local/tmp/pose_estimation
