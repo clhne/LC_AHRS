6ed4530312abba4dd93533877e84963b04f4b0d0
lucl1 <luchenglin@leadcoretech.com>
2018-07-11 14:20:52 +0800

    [add] delivery v1.0

---
+++ stm32f103c8t6_icm20602_code&doc/door_open_close_detection_20180710_v2.0/Objects/ahrs.d
@@ -0,0 +1,3 @@
+.\objects\ahrs.o: SRC\AHRS.c
+.\objects\ahrs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\ahrs.o: SRC\AHRS.h
---
+++ stm32f103c8t6_icm20602_code&doc/door_open_close_detection_20180710_v2.0/SRC/doordetection.h
@@ -0,0 +1,12 @@
+#ifndef __DOORDETECTION_H
+#define __DOORDETECTION_H
+#define DOOR_PEAK_TROUGH_VALUE_LITTLE -2
+#define NOT_READY -1
+#define READY 0
+#define DOOR_STATUS_OPEN 1
+#define DOOR_STATUS_CLOSE 2
+#define DOOR_STATUS_DETECTING 3
+void Door_Detection_Init(void);
+void Door_Detection_Reset(void);
+int  Door_Detection(short ax_adc, short ay_adc, short az_adc, short gx_adc, short gy_adc, short gz_adc, int *door_status, float *pitch, float *cur_cor_gx, long long ts);
+#endif
